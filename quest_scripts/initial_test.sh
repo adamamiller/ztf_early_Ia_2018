7367467e769ef27cf81c8e1749b9bb77845c59a3
Adam Miller <amiller.astro@gmail.com>
2019-02-22 11:53:54 -0600

    MAINT: correct absolute path to python script

--- quest_scripts/initial_test.sh
+++ quest_scripts/initial_test.sh
@@ -14,8 +14,6 @@ export PATH=$PATH:/projects/p30796/tools/
 module load python/anaconda
 source activate emcee3
 
-# Set your working directory
-cd $PBS_O_WORKDIR/software/adamamiller_git_clones/ztf_early_Ia_2018/playground
-
-# Another command you actually want to execute, if needed:
+# Run the actual commands to be executed
+cd /home/aam3503/software/adamamiller_git_clones/ztf_early_Ia_2018/playground
 python dummy_run_script.py
