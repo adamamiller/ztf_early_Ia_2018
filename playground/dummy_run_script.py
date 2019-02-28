c7359a240cce4ec281bf9adc31a2d3ab199b04ee
Adam Miller <amiller.astro@gmail.com>
2019-02-28 13:29:13 -0600

    MAINT: correct function default input parameters

--- playground/dummy_run_script.py
+++ playground/dummy_run_script.py
@@ -12,4 +12,4 @@ salt_df = pd.read_csv(data_path + "MB_SALT_020419.csv")
 t0 = float(salt_df['t0'][salt_df['sn'] == sn].values) + 2400000.5
 z = float(salt_df['z'][salt_df['sn'] == sn].values)
 
-fit_ztf_lc.fit_lc(lc_df, t0=0, z=0, t_fl=18)
+fit_ztf_lc.fit_lc(lc_df, t0=t0, z=z, t_fl=18)
