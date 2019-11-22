a64d4b5c0b1d69c375bcb9f716384f3f4500dcbf
Adam Miller <amiller.astro@gmail.com>
2019-11-22 15:31:57 -0600

    ENH: accomodate strict prior on alpha

--- playground/cv_kde_bandwidth.py
+++ playground/cv_kde_bandwidth.py
@@ -12,6 +12,7 @@ def get_all_bandwidths(h5_file,
                        thin_by=100, 
                        data_path = '',
                        n_cores=8,
+                       prior='uniformed'
                        **kwargs):
     '''optimal bandwidth for marginilized KDEs
     
@@ -27,33 +28,37 @@ def get_all_bandwidths(h5_file,
                                  thin=np.max([int(np.max(tau)), 1]), 
                                  flat=True)
     t_fl = samples[:,0]
-    alpha_g = samples[:,2]
-    alpha_r = samples[:,4]
-    delta_alpha = alpha_r - alpha_g
 
     time_bw = opt_bandwidth(t_fl, 
                             log_min_grid=-2.7,
                             log_max_grid=-0.3,
                             n_jobs=n_cores)
-    alpha_g_bw = opt_bandwidth(alpha_g, 
-                               log_min_grid=-2.7,
-                               log_max_grid=-0.3,
-                               n_jobs=n_cores)
-    alpha_r_bw = opt_bandwidth(alpha_r, 
-                               log_min_grid=-2.7,
-                               log_max_grid=-0.3,
-                               n_jobs=n_cores)
-    delta_alpha_bw = opt_bandwidth(delta_alpha, 
+    if prior == 'uniformed':
+        alpha_g = samples[:,2]
+        alpha_r = samples[:,4]
+        delta_alpha = alpha_r - alpha_g
+
+
+        alpha_g_bw = opt_bandwidth(alpha_g, 
                                    log_min_grid=-2.7,
                                    log_max_grid=-0.3,
                                    n_jobs=n_cores)
+        alpha_r_bw = opt_bandwidth(alpha_r, 
+                                   log_min_grid=-2.7,
+                                   log_max_grid=-0.3,
+                                   n_jobs=n_cores)
+        delta_alpha_bw = opt_bandwidth(delta_alpha, 
+                                       log_min_grid=-2.7,
+                                       log_max_grid=-0.3,
+                                       n_jobs=n_cores)
     
     sn = h5_file.split('/')[-1].split('_')[0]
-    with open(data_path + '{}_bandwidth.txt'.format(sn), 'w') as fw:
+    with open(data_path + '{}_{}_bandwidth.txt'.format(sn, prior), 'w') as fw:
         print('{} = bw for time_fl'.format(time_bw))
-        print('{} = bw for alpha_g'.format(alpha_g_bw))
-        print('{} = bw for alpha_r'.format(alpha_r_bw))
-        print('{} = bw for delta_alpha'.format(delta_alpha_bw))
+        if prior == 'uniformed':
+            print('{} = bw for alpha_g'.format(alpha_g_bw))
+            print('{} = bw for alpha_r'.format(alpha_r_bw))
+            print('{} = bw for delta_alpha'.format(delta_alpha_bw))
     
     return
 
@@ -78,10 +83,19 @@ def opt_bandwidth(marg_samples,
 if __name__== "__main__":
     ztf_name = str(sys.argv[1])
     n_cores = 8
+    prior = 'uniformed'
     if len(sys.argv) > 2:
         n_cores = int(sys.argv[2])
+    if len(sys.argv) > 3:
+        prior = 'delta2'
     
     data_path = "/projects/p30796/ZTF/early_Ia/forced_lightcurves/sample_lc_v2/big_unc/"
-    backend_filename = data_path + "/{}_emcee_40_varchange.h5".format(ztf_name)
-
-    get_all_bandwidths(backend_filename, data_path, n_cores=n_cores)
+    
+    if prior == 'uniformed':
+        backend_filename = data_path + "/{}_emcee_40_varchange.h5".format(ztf_name)
+        get_all_bandwidths(backend_filename, data_path, n_cores=n_cores)
+    elif prior == 'delta2':
+        backend_filename = data_path + "/{}_emcee_40_tsquared.h5".format(ztf_name)
+        get_all_bandwidths(backend_filename, data_path, 
+                           n_cores=n_cores, prior=prior)
+        
